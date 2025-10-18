f0bbfd389bd7fa30c98b5386c705502be522b392
miski21 <turjarab@gmail.com>
2025-10-17 22:28:35 -0500

    factorial

--- src/main/java/pe/edu/tecsup/app/Main.java
+++ src/main/java/pe/edu/tecsup/app/Main.java
@@ -20,4 +20,11 @@ public class Main {
         }
         return suma;
     }
+    public static int obtenerFactorial(int n) {
+        int factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
 }
